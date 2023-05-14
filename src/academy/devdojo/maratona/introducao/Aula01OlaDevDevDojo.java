cec05e56aeb2cac4127cdae201eb5b9d3e9c9a22
flavioNevesjc <flaviosn@gmail.com>
2023-05-14 19:08:33 -0300

    Comite nas aulas de Java - DevDojo

---
+++ src/academy/devdojo/maratona/introducao/Aula01OlaDevDevDojo.java
@@ -0,0 +1,6 @@
+package academy.devdojo.maratona.introducao;
+public class Aula01OlaDevDevDojo {
+    public static void main (String[] args) {
+        System.out.println("Olá mundo");
+    }
+}
---
+++ src/academy/devdojo/maratona/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratona.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main (String[] args) {
+        // int, double float, char, byte, short, long, boolean
+        int age = 10;
+        System.out.println("Sua idade é " + age+" anos");
+    }
+}
